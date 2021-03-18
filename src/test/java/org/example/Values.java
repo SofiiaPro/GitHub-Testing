282a87aaa95cfac143ee3a9d044868613cfcb42b
SofiiaPro <54141095+SofiiaPro@users.noreply.github.com>
2021-03-18 19:12:15 +0200

    Editing

--- src/test/java/org/example/Values.java
+++ src/test/java/org/example/Values.java
@@ -2,7 +2,7 @@ package org.example;
 
 public interface Values {
     String login = "sofiahrn242@gmail.com";
-    String password = "Afanth39";
+    String password = "";
     String repositoryName = "TestRepository";
     String language = "Java";
     String readmeText = "Create README.md";
