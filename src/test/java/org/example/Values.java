cd8d3fcfbd52fc2452ad02bf90343d49549f3e50
SofiiaPro <54141095+SofiiaPro@users.noreply.github.com>
2021-03-18 19:25:28 +0200

    editing

---
+++ src/test/java/org/example/Values.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public interface Values {
+    String login = "sofiahrn242@gmail.com";
+    String password = "";
+    String repositoryName = "TestRepository";
+    String language = "Java";
+    String readmeText = "Create README.md";
+    String commitMessage = "Add readme file";
+    String deleteInputText = "SofiiaPro/TestRepository";
+    String PAGE_URL="https://github.com";
+}
