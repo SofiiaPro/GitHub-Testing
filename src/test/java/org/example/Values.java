39d3ed5c4517d30232f86ca28f38cfb4a821dc8d
Sofia Bendus <54141095+SofiiaPro@users.noreply.github.com>
2021-03-18 14:54:13 +0200

    Update Values.java

--- src/test/java/org/example/Values.java
+++ src/test/java/org/example/Values.java
@@ -1,12 +1,1 @@
 package org.example;
-
-public interface Values {
-    String login = "sofiahrn242@gmail.com";
-    String password = "Afanth39";
-    String repositoryName = "TestRepository";
-    String language = "Java";
-    String readmeText = "Create README.md";
-    String commitMessage = "Add readme file";
-    String deleteInputText = "SofiiaPro/TestRepository";
-    String PAGE_URL="https://github.com";
-}
